3f9edf0b6fefe00c26fe63461917778c4947ac9a
Vrunda <vrunda1002@gmail.com>
2023-12-16 17:24:12 +0530

    Add new post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const {check, validationResult} = require("express-validator/");
 
-// @route   GET api/posts
-// @desc    Test Route
-// @access  Public
-router.get("/", (req, res) => res.send("Posts Route"));
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+
+    try {
+      const user = await User.findById(req.user).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
