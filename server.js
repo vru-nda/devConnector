3680f00126a76241fcecf64c1a45c21fb4fdc8a4
Vrunda <vrunda1002@gmail.com>
2023-12-16 13:31:14 +0530

    Connect to DB and basic routes setup

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//connect to database
+connectDB();
+
+// Init middleware
+app.use(express.json({extended: false}));
+
 app.get("/", (req, res) => res.send("API running"));
+
+// define routes
+// app.use("/users", require("./routes/api/users"));
+app.use("/auth", require("./routes/api/auth"));
+app.use("/posts", require("./routes/api/posts"));
+app.use("/profile", require("./routes/api/profile"));
 
 const PORT = process.env.PORT || 5000;
 
